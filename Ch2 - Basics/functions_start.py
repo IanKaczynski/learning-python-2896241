14a781071517869d4ffb7af217344b288bf4192f
Ian Kaczynski <Ian_Kaczynski1@baylor.edu>
2025-01-28 18:52:55 -0600

    #	modified:   Ch2 - Basics/classes_start.py #	modified:   Ch2 - Basics/conditionals_start.py #	modified:   Ch2 - Basics/functions_start.py #	modified:   Ch2 - Basics/helloworld_start.py #	modified:   Ch2 - Basics/loops_start.py #	modified:   Ch2 - Basics/modules_start.py #	modified:   Ch2 - Basics/variables_start.py

--- Ch2 - Basics/functions_start.py
+++ Ch2 - Basics/functions_start.py
@@ -5,17 +5,51 @@
 
 
 # TODO: define a basic function
-
+def func1():
+    print("I am a function")
 
 # TODO: function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1," ",arg2)
 
 # TODO: function that returns a value
-
+def cube(x):
+    return x*x*x
 
 # TODO: function with default value for an argument
-
+def power(num, x=1): # x=1 is the default value
+    result = 1 # this is the default value
+    for i in range(x):
+        result = result * num
+    return result 
 
 # TODO: function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for x in args:
+        result = result + x
+    return result
 
 
+# func1() # this will print the function
+# print(func1()) # this will print the function and then None
+# print(func1) # this will print the memory location of the function
+
+# func2(10,20)
+# print(func2(10,20)) # this will print the function and then None
+# print(cube(3)) # this will print the cube of 3  
+# print(cube) # this will print the memory location of the function   
+
+# print(power(2)) # this will print the power of 2 to the power of 1
+# print(power (2,3)) # this will print the power of 2 to the power of 3   
+
+# print(power(x=3, num=2)) # this will print the power of 2 to the power of 3 
+# this is because we are passing the values of the arguments in the function    
+# in a different order but we are specifying the name of the arguments  
+
+print(multi_add(4,5,10,4)) # this will print the sum of the numbers passed in the function  
+# this is because we are passing the values of the arguments in the function    
+# in a different order but we are specifying the name of the arguments  
+# we are also passing multiple arguments in the function    
+# this is done by using the *args in the function definition    
+# this is called a variable number of arguments 
--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -13,25 +13,45 @@ mylist = [0, 1, "two", 3.2, False]
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
-print(myint)
-print(myfloat)
-print(mystr)
-print(mybool)
-print(mylist)
-print(mytuple)
-print(mydict)
+# print(myint)
+# print(myfloat)
+# print(mystr)
+# print(mybool)
+# print(mylist)
+# print(mytuple)
+# print(mydict)
 
 # re-declaring a variable works
+# myint = "abc"
+# print(myint)
 
 # to access a member of a sequence type, use []
+# print(mylist[2])
+# print(mytuple[1])
 
-# use slices to get parts of a sequence
+# # use slices to get parts of a sequence
+# print(mylist[1:5]) 
+# print(mylist[1:5:2]) # start, stop, step
 
-# you can use slices to reverse a sequence
+# # you can use slices to reverse a sequence
+# print(mylist[::-1]) #not specifing a start or stop, but only the step value which is telling python to go through the list in reverse order
 
 # dictionaries are accessed via keys
+#(dictionaries give you the item associated with the key)
+# print(mydict["one"]) #this would print the value associated with the key "one" in the dictionary mydict
 
 # ERROR: variables of different types cannot be combined
+# print("string type" + 123) #this would give a TYPE error because you cannot concatenate a string and an integer
+# print("string type" + str(123)) #this would work because you are converting the integer to a string before concatenating it with the string
 
 # Global vs. local variables in functions
+def someFunction():
+    global mystr #this would make mystr a global variable and it can be accessed outside the function
+    mystr = "def"
+    print(mystr)
 
+someFunction() #this would print the value of mystr in the function someFunction
+print(mystr) #this would give an error because mystr is a local variable in the function someFunction and cannot be accessed outside the function
+
+del mystr #this would delete the variable mystr
+print(mystr) #this would give an error because mystr has been deleted and cannot be accessed
